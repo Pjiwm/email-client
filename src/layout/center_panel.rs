f043a25f93610e6b7abe9026fc0e282d681ec195
Pjiwm <pim.munne@gmail.com>
2022-05-01 22:30:21 +0200

    made login screen

--- src/layout/center_panel.rs
+++ src/layout/center_panel.rs
@@ -1,21 +1,37 @@
 use gtk::{
     traits::{ContainerExt, WidgetExt},
-    Align, Button, Orientation, SearchBar,
+    Align, Button, InputPurpose, Orientation,
 };
 
 pub fn center_box() -> gtk::Box {
-    let parent = gtk::Box::new(Orientation::Horizontal, 1);
+    let parent = gtk::Box::new(Orientation::Vertical, 1);
     let email = gtk::Entry::builder()
-    .halign(Align::Center)
-    .margin_top(10)
-    .margin_bottom(2)
-    .build();
-    email.show_all();
-    let submit_btn = Button::builder()
-        .label("Submit")
+        .halign(Align::Center)
+        .placeholder_text("Example@example.com")
+        .margin_top(10)
+        .width_request(200)
+        .input_purpose(InputPurpose::Email)
         .build();
+    email.show_all();
+
+    let password = gtk::Entry::builder()
+        .halign(Align::Center)
+        .placeholder_text("Password")
+        .margin_top(5)
+        .margin_bottom(2)
+        .width_request(200)
+        .input_purpose(InputPurpose::Password)
+        .visibility(false)
+        .build();
+    password.show_all();
+
+    let submit_btn = Button::builder().width_request(200).label("Submit").build();
     submit_btn.show_all();
+
     parent.add(&email);
+    parent.add(&password);
     parent.add(&submit_btn);
+    parent.set_height_request(600);
+    parent.set_width_request(500);
     parent
 }
